66b431b55a7439660b0b2f8fa3613ef87848a90a
Divyendu Singh <divyendu@xata.io>
2025-06-18 17:39:53 +0200

    install xata directly

--- build.sh
+++ build.sh
@@ -3,10 +3,14 @@ set -e
 
 env
 
-apt install -y jq
-curl -fsSL https://xata.io/install.sh | bash
+# apt install -y jq
+# curl -fsSL https://xata.io/install.sh | bash
 
-xata --version || true
+curl -o xata https://xata-tech-downloads.s3.amazonaws.com/channels/latest/manifest.json
+chmod +x xata
+mv xata /usr/local/bin/xata
+
+xata --version
 
 node build.js
 next build
