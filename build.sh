93f097b7ca0daa23143435e06d64c1831d063b10
Divyendu Singh <divyendu@xata.io>
2025-06-18 17:44:22 +0200

    fix build.sh

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ env
 # apt install -y jq
 # curl -fsSL https://xata.io/install.sh | bash
 
-curl -o xata https://xata-tech-downloads.s3.amazonaws.com/channels/latest/manifest.json
-chmod +x xata
-mv xata /usr/local/bin/xata
+curl -o xata https://xata-cli-versions.s3.amazonaws.com/versions/1.0.42-latest/xata-linux-x64
+chmod +x ./xata
+mv ./xata /usr/local/bin/xata
 
 xata --version
 
