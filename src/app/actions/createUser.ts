4c76a6e36c9fb722c114c8070bf41bb649bcc4d1
Divyendu Singh <divyendu@xata.io>
2025-06-19 17:22:47 +0200

    remove invariant

--- src/app/actions/createUser.ts
+++ src/app/actions/createUser.ts
@@ -1,11 +1,11 @@
 "use server";
 
 import postgres from "postgres";
-import invariant from "tiny-invariant";
+// import invariant from "tiny-invariant";
 
 export async function createUser() {
-  invariant(process.env.DATABASE_URL, "DATABASE_URL is not set");
-  const sql = postgres(process.env.DATABASE_URL);
+  //   invariant(process.env.DATABASE_URL, "DATABASE_URL is not set");
+  const sql = postgres(process.env.DATABASE_URL!);
   const users =
     await sql`insert into users (id, name) values (1, 'Alice from branch') on conflict do nothing returning *`;
   return users[0];
