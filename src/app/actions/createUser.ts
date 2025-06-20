51b42df493343e3f1f936cd84d4c7ea09bfde4db
Divyendu Singh <divyendu@xata.io>
2025-06-20 07:45:13 +0200

    use id

--- src/app/actions/createUser.ts
+++ src/app/actions/createUser.ts
@@ -7,7 +7,7 @@ export async function createUser(id: number, name: string) {
   invariant(process.env.DATABASE_URL, "DATABASE_URL is not set");
 
   const sql = postgres(process.env.DATABASE_URL!);
-  const users = await sql`insert into users (id, name) values (1, ${
+  const users = await sql`insert into users (id, name) values (${id}, ${
     name + " from branch"
   }) on conflict (id) do update set name = excluded.name returning *`;
   return users[0];
